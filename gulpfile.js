956083f5fc9f6863167663c7f7f73524a7cee32a
Kevin Coyle <kevin.coyle@drutek.co.uk>
2015-05-01 00:30:29 +0100

    Playing with some styling

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 var browserSync = require('browser-sync');
+var swig = require('gulp-swig');
+
 gulp.task('sass', function() {
   gulp.src('./sass/style.scss')
   .pipe(sass())
@@ -9,22 +11,33 @@ gulp.task('sass', function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./sass/**/*', ['sass'])
+  gulp.watch('./sass/**/*', ['sass']);
+  gulp.watch('./twig/**/*', ['twig']);
 });
 
-gulp.task('default',['sass', 'watch', 'browser-sync'], function() {
+gulp.task('twig', function() {
+  gulp.src('./twig/pages/*')
+  .pipe(swig({
+    defaults: {
+      cache: false
+    }
+  }))
+  .pipe(gulp.dest('./build'));
+})
+
+gulp.task('default',['sass','twig', 'watch', 'browser-sync'], function() {
 
 })
 
 gulp.task('browser-sync', function () {
    var files = [
       './sass/**/*',
-      'index.html',
+      './twig/**/*',
    ];
 
    browserSync.init(files, {
       server: {
-         baseDir: './'
+         baseDir: './build'
       }
    });
 });
